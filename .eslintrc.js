59dee67aa0357c4b776cf5db9c62f3712eaafe30
Gustavo Eklund <gustavo04616@gmail.com>
2020-11-05 20:21:19 -0300

    removed: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ['airbnb', 'plugin:prettier/recommended'],
+    extends: ['airbnb', 'plugin:react/recommended'],
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
