fa2771aca215c660990a3abf803b810119d81cff
Gustavo Eklund <gustavo04616@gmail.com>
2020-11-05 21:54:31 -0300

    added: api service

---
+++ src/services/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { getToken, getUserData, logout } from './authentication';
+
+const api = axios.create({
+    baseURL: 'https://localhost/ucycle/ucycle-backend',
+});
+
+api.interceptors.request.use(async (config) => {
+    const token = getToken();
+    const modifiedConfig = config;
+
+    if (token) {
+        const userData = getUserData();
+        const todayPlusLifetime = new Date(new Date().getTime() + 3 * 24 * 60 * 60);
+        const expireDate = new Date(userData.exp * 1000);
+
+        // Expired token
+        if (todayPlusLifetime > expireDate) {
+            logout();
+            return modifiedConfig;
+        }
+
+        modifiedConfig.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return modifiedConfig;
+});
+
+export default api;
