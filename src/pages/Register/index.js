3accd7b290e1709f302791f14e32ca92265f6437
Gustavo Eklund <gustavo04616@gmail.com>
2020-11-19 20:35:45 -0300

    apply register functionality

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,45 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Button,
     Grid,
     Link,
-    OutlinedInput,
+    Typography,
+    TextField,
     FormControlLabel,
     Checkbox,
+    Slide,
+    Snackbar,
+    Backdrop,
+    CircularProgress,
 } from '@material-ui/core';
-import { Link as RouterDomLink } from 'react-router-dom';
+import Alert from '@material-ui/lab/Alert';
+import { Link as RouterDomLink, useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+import validation from '../../services/validation';
 import useStyles from './styles';
+import api from '../../services/api';
 
 export default function Register() {
+    const {
+        register,
+        errors,
+        setError,
+        handleSubmit,
+    } = useForm();
     const classes = useStyles();
+    const history = useHistory();
+    const [isLoading, setIsLoading] = useState(false);
+    const [snackbar, setSnackbar] = useState({ isShowing: false, message: '' });
+
+    async function handleRegister(formData) {
+        setIsLoading(true);
+
+        try {
+            if (formData.password !== formData.password_repeat) {
+                setError('password_repeat', {
+                    type: 'validation',
+                    message: 'As senhas não coincidem.',
+                });
+
+                return;
+            }
+
+            const response = await api.post('/authentication/register', formData);
+
+            const { error } = response.data;
+
+            if (error) {
+                throw new Error(error.message);
+            }
+
+            history.push('/login');
+        } catch (error) {
+            setSnackbar({ isShowing: true, message: error.message });
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    function handleCloseAlert() {
+        setSnackbar({ ...snackbar, isShowing: false });
+    }
 
     return (
         <Grid container>
             <Grid item xs={1} />
-            <Grid item xs={10} className={classes.container}>
-                <OutlinedInput
-                    className={classes.input}
+            <Grid
+                item
+                xs={10}
+                component="form"
+                className={classes.container}
+                onSubmit={handleSubmit(handleRegister)}
+            >
+                <TextField
                     fullWidth
-                    placeholder="Nome completo"
+                    type="text"
+                    variant="outlined"
+                    label="Nome completo"
+                    id="full_name"
+                    name="full_name"
+                    inputRef={register(validation.full_name)}
+                    error={Boolean(errors.full_name)}
+                    className={classes.input}
+                    helperText={errors.full_name && errors.full_name.message}
                 />
-                <OutlinedInput
-                    className={classes.input}
+                <TextField
                     fullWidth
-                    placeholder="Email"
+                    variant="outlined"
+                    label="Email"
+                    name="email"
+                    id="email"
+                    inputRef={register(validation.email)}
+                    error={Boolean(errors.email)}
+                    className={classes.input}
+                    helperText={errors.email && errors.email.message}
                 />
-                <OutlinedInput
-                    className={classes.input}
+                <TextField
                     fullWidth
+                    variant="outlined"
                     type="password"
-                    placeholder="Senha"
-                />
-                <OutlinedInput
+                    label="Senha"
+                    name="password"
+                    id="password"
+                    inputRef={register(validation.password)}
+                    error={Boolean(errors.password)}
                     className={classes.input}
-                    fullWidth
-                    type="password"
-                    placeholder="Confirmar senha"
+                    helperText={errors.password && errors.password.message}
                 />
-
+                <TextField
+                    fullWidth
+                    variant="outlined"
+                    type="password"
+                    label="Confirmar senha"
+                    name="password_repeat"
+                    inputRef={register(validation.password_repeat)}
+                    error={Boolean(errors.password_repeat)}
+                    className={classes.input}
+                    helperText={errors.password_repeat && errors.password_repeat.message}
+                />
                 <FormControlLabel
                     control={(
                         <Checkbox
@@ -47,6 +128,7 @@ export default function Register() {
                             // onChange={}
                             value="checkedB"
                             color="primary"
+                            name="news"
                         />
                     )}
                     label={(
@@ -62,8 +144,9 @@ export default function Register() {
                         <Checkbox
                             // checked={}
                             // onChange={}
-                            value="checkedB"
+                            value="checkedA"
                             color="primary"
+                            name="therms"
                         />
                     )}
                     label={(
@@ -86,6 +169,7 @@ export default function Register() {
                     color="primary"
                     fullWidth
                     size="large"
+                    type="submit"
                     className={classes.button}
                 >
                     Criar Conta
@@ -99,6 +183,24 @@ export default function Register() {
                 </Link>
             </Grid>
             <Grid item xs={1} />
+            <Snackbar
+                open={snackbar.isShowing}
+                TransitionComponent={Slide}
+                onClose={handleCloseAlert}
+            >
+                <Alert severity="error" onClose={handleCloseAlert}>
+                    {snackbar.message}
+                </Alert>
+            </Snackbar>
+            <Backdrop
+                open={isLoading}
+                style={{ zIndex: 1000, flexDirection: 'column', color: '#ffffff' }}
+            >
+                <CircularProgress />
+                <Typography variant="h5">
+                    &nbsp;&nbsp;Cadastrando...
+                </Typography>
+            </Backdrop>
         </Grid>
     );
 }
